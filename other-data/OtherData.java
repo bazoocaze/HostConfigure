c9bd8d39a72f0a74613f9bba0bae9d654c7a8abc
Jose Ferreira <bazoocaze@gmail.com>
2021-05-12 18:08:03 -0300

    Other data file

---
+++ other-data/OtherData.java
@@ -0,0 +1,10 @@
+public class OtherData {
+   /*
+      a3lmx7bdruulsm3pvh1f55k44@canarytokens.com
+      2xbftxnkdwf3ywda2lbehip7e@canarytokens.com
+      epaminondas.castilhos@axur.com
+   */
+	public static void main(String[] args) {
+		System.out.println("2xbftxnkdwf3ywda2lbehip7e@canarytokens.com");
+	}
+}
